2211abda46176ec194b3e8cc25626b4f57397bc6
anadulimited@gmail.com <anadulimited@gmail.com>
2025-11-09 23:02:37 +0100

    Refactor donation retrieval to include only successful donations and streamline error handling

--- controllers/donorController.js
+++ controllers/donorController.js
@@ -28,7 +28,8 @@ exports.totalStudentsHelped = async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({
-      message: `Error getting total students helped: ${error.message}`,
+      message: `Internal server error getting total students helped`,
+      error: error.message,
     });
   }
 };
@@ -37,8 +38,9 @@ exports.myDonations = async (req, res) => {
   try {
     const { donorId } = req.params;
     const donations = await paymentModel
-      .find({ senderId: donorId })
-      .populate("receiverId campaignId");
+      .find({ senderId: donorId, status: "successful" })
+      .populate("receiverId campaignId")
+      .exec();
 
     const total = donations.length;
     return res.status(200).json({
@@ -47,32 +49,33 @@ exports.myDonations = async (req, res) => {
       data: donations,
     });
   } catch (error) {
-    return res
-      .status(500)
-      .json({
-        message: `Error getting all my donations`,
-        error: error.message,
-      });
+    return res.status(500).json({
+      message: `Internal server error getting all my donations`,
+      error: error.message,
+    });
   }
 };
 
 exports.getDonorsForStudent = async (req, res) => {
   try {
     const { studentId } = req.params;
-    const donors = await campaignModel.find({ studentId }).distinct("donorId");
-    if (!donors || donors.length === 0) {
-      return res.status(404).json({ message: "No donor yet" });
-    }
+    const donors = await paymentModel
+      .find({ receiverId: studentId, status: "successful" })
+      .populate("senderId")
+      .exec();
+    const total = donors.length;
 
-    if (donors) {
-      return res.status(200).json({
-        message: `${donors.length} generous supporters`,
-        data: donors,
-      });
-    }
+    return res.status(200).json({
+      message: total < 1 ? "No donations hey" : "Donors found successfully",
+      total,
+      data: donors,
+    });
   } catch (error) {
     return res
       .status(500)
-      .json({ message: `Error getting donors: ${error.message}` });
+      .json({
+        message: `Internal server error getting donors`,
+        error: error.message,
+      });
   }
 };
