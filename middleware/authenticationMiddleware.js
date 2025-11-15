054cfdc7b6c06f791c27e5d5a21defb0f24ff0ab
anadulimited@gmail.com <anadulimited@gmail.com>
2025-11-15 08:58:23 +0100

    Refactor authentication middleware to redirect users on authentication failures

--- middleware/authenticationMiddleware.js
+++ middleware/authenticationMiddleware.js
@@ -2,21 +2,26 @@ const userModel = require("../models/userModel");
 const jwt = require("jsonwebtoken");
 const { verifyJwt } = require("../utils/jwtUtil");
 
+const loginUrl = `https://edu-fund-gamma.vercel.app/login`;
+const registerUrl = `https://edu-fund-gamma.vercel.app/account-type`;
+
 exports.isAuthenticated = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      return res.status(401).json({
-        message: "User not authenticated, please login",
-      });
+      // return res.status(401).json({
+      //   message: "User not authenticated, please login",
+      // });
+      return res.redirect(303, loginUrl);
     }
     const decoded = await verifyJwt(token);
     const user = await userModel.findById(decoded.id);
 
     if (!user) {
-      return res.status(404).json({
-        message: "Cannot perform action please create an account",
-      });
+      // return res.status(404).json({
+      //   message: "Cannot perform action please create an account",
+      // });
+      return res.redirect(303, registerUrl);
     }
     if (!user.isVerified) {
       return res.status(403).json({
@@ -29,9 +34,10 @@ exports.isAuthenticated = async (req, res, next) => {
     next();
   } catch (error) {
     if (error instanceof jwt.TokenExpiredError) {
-      return res.status(401).json({
-        message: "Session expired, Please login again to continue",
-      });
+      // return res.status(401).json({
+      //   message: "Session expired, Please login again to continue",
+      // });
+      return res.redirect(303, loginUrl);
     }
     res.status(500).json({
       message: error.message,
